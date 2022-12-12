3afb50f69f796bade1fec900c918d92ec748a5d9
Alla Kruglova <renata-l@yandex.ru>
2022-12-12 23:26:50 +0300

    v1

--- index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-multiple-empty-lines */
-/* eslint-disable linebreak-style */
-import './src/css/style.css';
-
-import './src/js/app';
-import './src/tickets';
-
