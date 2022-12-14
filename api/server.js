52d533618b7fd969af9b0fbfa07b8809fe7e1c06
Alla Kruglova <renata-l@yandex.ru>
2022-12-14 19:42:36 +0300

    16/12

--- api/server.js
+++ api/server.js
@@ -41,7 +41,7 @@ app.use(koaBody({
 // );
 
 // const port = process.env.PORT || 7070;
-const port = 7070;
+const port = process.env.PORT || 7070;
 // const port = process.env.API_URL || 7070;
 console.log(`proc - ${process.env.PORT}, ${process.env}, `);
 
